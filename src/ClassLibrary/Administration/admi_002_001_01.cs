720d0b4f54dd8dd8180e1311e433f5ff317c4f17
cep.develop <cep.develop@1e891f1c-c86b-11dd-9042-6d746b85d38b>
2009-01-04 20:01:16 +0000

    fix documentation bugs

--- src/ClassLibrary/Administration/admi_002_001_01.cs
+++ src/ClassLibrary/Administration/admi_002_001_01.cs
@@ -1,25 +1,25 @@
 
-//                                      ______    ____  
-//                                     |____  | "|__  | 
-//                                      _   | |     | | 
-//                                     | |  | |   __| | 
+//                                      ______    ____   
+//                                     |____  | "|__  |  
+//                                      _   | |     | |  
+//                                     | |  | |   __| |  
 //                                     |_|  |_|  |_____| 
-//
-//  --------------------------------------------------- 
-// |     _   ____     ___     ___    ____    ____      |
-// |    (_) |___ \   / _ \   / _ \  |___ \  |___ \     |
-// |    | |   __) | | | | | | | | |   __) |   __) |    |
-// |    | |  / __/  | |_| | | |_| |  / __/   / __/     |
-// |    |_| |_____|  \___/   \___/  |_____| |_____|    |
-// |                                                   |
-// |    AN ISO 20022 OPEN-SOURCE .NET CLASS LIBRARY    |
-// |            FOR MESSAGING APPLICATIONS             |
-// |                                                   |
-// |        Copyright (C) 2008 Mauricio Leventer       |
-// |                mleventer@i20022.com               |
-// |                                                   |
-//  ---------------------------------------------------
-//
+//                                                       
+//  ---------------------------------------------------  
+// |     _   ____     ___     ___    ____    ____      | 
+// |    (_) |___ \   / _ \   / _ \  |___ \  |___ \     | 
+// |    | |   __) | | | | | | | | |   __) |   __) |    | 
+// |    | |  / __/  | |_| | | |_| |  / __/   / __/     | 
+// |    |_| |_____|  \___/   \___/  |_____| |_____|    | 
+// |                                                   | 
+// |    AN ISO 20022 OPEN-SOURCE .NET CLASS LIBRARY    | 
+// |            FOR MESSAGING APPLICATIONS             | 
+// |                                                   | 
+// |        Copyright (C) 2008 Mauricio Leventer       | 
+// |                mleventer@i20022.com               | 
+// |                                                   | 
+//  ---------------------------------------------------  
+//                                                       
 // L I C E N S E                                         
 //                                                       
 // This program is free software; you can redistribute it
@@ -39,7 +39,7 @@
 // to the Free Software Foundation, Inc.,                
 // 59 Temple Place, Suite 330, Boston, MA                
 // 02111-1307 USA.                                       
-//                                                       
+//
 //
 using System;
 using System.Collections.Generic;
@@ -49,11 +49,11 @@ using System.Text;
 using System.Xml.Serialization;
 
 
+
 namespace i20022.admi00200101
 {
-
-
-
+    
+    
     /// <summary>
     /// <b>Document</b> is the root class of the ISO 20022 message
     /// with id admi.002.001.01 name MessageRejectV01 from Administration business area.
@@ -61,99 +61,99 @@ namespace i20022.admi00200101
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.002.001.01" )]
-    [XmlRootAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.002.001.01", IsNullable = false )]
+    [XmlTypeAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.002.001.01")]
+    [XmlRootAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.002.001.01", IsNullable=false)]
     public class Document
     {
-
+        
         /// <summary>
-        /// Element <b>admi00200101</b> (0020 01 01) of type admi00200101.
+        /// Element <b>admi00200101</b> (admi 00200101) of type admi00200101.
         /// </summary>
-        [XmlElementAttribute( "admi.002.001.01", Order = 0 )]
+        [XmlElementAttribute("admi.002.001.01", Order=0)]
         public admi00200101 admi00200101;
     }
-
+    
     /// <summary>
-    /// Class <b>admi00200101</b> (0020 01 01).
+    /// Class <b>admi00200101</b> (admi 00200101).
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( TypeName = "admi.002.001.01", Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.002.001.01" )]
+    [XmlTypeAttribute(TypeName="admi.002.001.01", Namespace="urn:iso:std:iso:20022:tech:xsd:admi.002.001.01")]
     public class admi00200101
     {
-
+        
         /// <summary>
         /// Element <b>RltdRef</b> (Related Reference) of type MessageReference.
         /// </summary>
-        [XmlElementAttribute( Order = 0 )]
+        [XmlElementAttribute(Order=0)]
         public MessageReference RltdRef;
-
+        
         /// <summary>
         /// Element <b>Rsn</b> (Reason) of type RejectionReason2.
         /// </summary>
-        [XmlElementAttribute( Order = 1 )]
+        [XmlElementAttribute(Order=1)]
         public RejectionReason2 Rsn;
     }
-
+    
     /// <summary>
     /// Class <b>MessageReference</b> (Message Reference).
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.002.001.01" )]
+    [XmlTypeAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.002.001.01")]
     public class MessageReference
     {
-
+        
         /// <summary>
         /// Element <b>Ref</b> (Reference) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 0 )]
+        [XmlElementAttribute(Order=0)]
         public string Ref;
     }
-
+    
     /// <summary>
     /// Class <b>RejectionReason2</b> (Rejection Reason 2).
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.002.001.01" )]
+    [XmlTypeAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.002.001.01")]
     public class RejectionReason2
     {
-
+        
         /// <summary>
         /// Element <b>RjctgPtyRsn</b> (Rejecting Party Reason) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 0 )]
+        [XmlElementAttribute(Order=0)]
         public string RjctgPtyRsn;
-
+        
         /// <summary>
         /// Element <b>RjctnDtTm</b> (Rejection Date Time) of type System.DateTime.
         /// </summary>
-        [XmlElementAttribute( Order = 1 )]
+        [XmlElementAttribute(Order=1)]
         public System.DateTime RjctnDtTm;
-
+        
         /// <summary>
         /// Element <b>RjctnDtTmSpecified</b> (Rejection Date Time Specified) of type bool.
         /// </summary>
-        [XmlIgnoreAttribute()]
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
         public bool RjctnDtTmSpecified;
-
+        
         /// <summary>
         /// Element <b>ErrLctn</b> (Error Location) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 2 )]
+        [XmlElementAttribute(Order=2)]
         public string ErrLctn;
-
+        
         /// <summary>
         /// Element <b>RsnDesc</b> (Reason Description) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 3 )]
+        [XmlElementAttribute(Order=3)]
         public string RsnDesc;
-
+        
         /// <summary>
         /// Element <b>AddtlData</b> (Additional Data) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 4 )]
+        [XmlElementAttribute(Order=4)]
         public string AddtlData;
     }
 }
--- src/ClassLibrary/Administration/admi_004_001_01.cs
+++ src/ClassLibrary/Administration/admi_004_001_01.cs
@@ -1,25 +1,25 @@
 
-//                                      ______    ____  
-//                                     |____  | "|__  | 
-//                                      _   | |     | | 
-//                                     | |  | |   __| | 
+//                                      ______    ____   
+//                                     |____  | "|__  |  
+//                                      _   | |     | |  
+//                                     | |  | |   __| |  
 //                                     |_|  |_|  |_____| 
-//
-//  --------------------------------------------------- 
-// |     _   ____     ___     ___    ____    ____      |
-// |    (_) |___ \   / _ \   / _ \  |___ \  |___ \     |
-// |    | |   __) | | | | | | | | |   __) |   __) |    |
-// |    | |  / __/  | |_| | | |_| |  / __/   / __/     |
-// |    |_| |_____|  \___/   \___/  |_____| |_____|    |
-// |                                                   |
-// |    AN ISO 20022 OPEN-SOURCE .NET CLASS LIBRARY    |
-// |            FOR MESSAGING APPLICATIONS             |
-// |                                                   |
-// |        Copyright (C) 2008 Mauricio Leventer       |
-// |                mleventer@i20022.com               |
-// |                                                   |
-//  ---------------------------------------------------
-//
+//                                                       
+//  ---------------------------------------------------  
+// |     _   ____     ___     ___    ____    ____      | 
+// |    (_) |___ \   / _ \   / _ \  |___ \  |___ \     | 
+// |    | |   __) | | | | | | | | |   __) |   __) |    | 
+// |    | |  / __/  | |_| | | |_| |  / __/   / __/     | 
+// |    |_| |_____|  \___/   \___/  |_____| |_____|    | 
+// |                                                   | 
+// |    AN ISO 20022 OPEN-SOURCE .NET CLASS LIBRARY    | 
+// |            FOR MESSAGING APPLICATIONS             | 
+// |                                                   | 
+// |        Copyright (C) 2008 Mauricio Leventer       | 
+// |                mleventer@i20022.com               | 
+// |                                                   | 
+//  ---------------------------------------------------  
+//                                                       
 // L I C E N S E                                         
 //                                                       
 // This program is free software; you can redistribute it
@@ -39,7 +39,7 @@
 // to the Free Software Foundation, Inc.,                
 // 59 Temple Place, Suite 330, Boston, MA                
 // 02111-1307 USA.                                       
-//                                                       
+//
 //
 using System;
 using System.Collections.Generic;
@@ -49,9 +49,11 @@ using System.Text;
 using System.Xml.Serialization;
 
 
+
 namespace i20022.admi00400101
 {
-
+    
+    
     /// <summary>
     /// <b>Document</b> is the root class of the ISO 20022 message
     /// with id admi.004.001.01 name SystemEventNotificationV01 from Administration business area.
@@ -59,71 +61,71 @@ namespace i20022.admi00400101
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.004.001.01" )]
-    [XmlRootAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.004.001.01", IsNullable = false )]
+    [XmlTypeAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.004.001.01")]
+    [XmlRootAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.004.001.01", IsNullable=false)]
     public class Document
     {
-
+        
         /// <summary>
-        /// Element <b>admi00400101</b> (0040 01 01) of type admi00400101.
+        /// Element <b>admi00400101</b> (admi 00400101) of type admi00400101.
         /// </summary>
-        [XmlElementAttribute( "admi.004.001.01", Order = 0 )]
+        [XmlElementAttribute("admi.004.001.01", Order=0)]
         public admi00400101 admi00400101;
     }
-
+    
     /// <summary>
-    /// Class <b>admi00400101</b> (0040 01 01).
+    /// Class <b>admi00400101</b> (admi 00400101).
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( TypeName = "admi.004.001.01", Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.004.001.01" )]
+    [XmlTypeAttribute(TypeName="admi.004.001.01", Namespace="urn:iso:std:iso:20022:tech:xsd:admi.004.001.01")]
     public class admi00400101
     {
-
+        
         /// <summary>
         /// Element <b>EvtInf</b> (Event Information) of type Event1.
         /// </summary>
-        [XmlElementAttribute( Order = 0 )]
+        [XmlElementAttribute(Order=0)]
         public Event1 EvtInf;
     }
-
+    
     /// <summary>
     /// Class <b>Event1</b> (Event 1).
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
-    [XmlTypeAttribute( Namespace = "urn:iso:std:iso:20022:tech:xsd:admi.004.001.01" )]
+    [XmlTypeAttribute(Namespace="urn:iso:std:iso:20022:tech:xsd:admi.004.001.01")]
     public class Event1
     {
-
+        
         /// <summary>
         /// Element <b>EvtCd</b> (Event Code) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 0 )]
+        [XmlElementAttribute(Order=0)]
         public string EvtCd;
-
+        
         /// <summary>
         /// Element <b>EvtParam</b> (Event Parameter) of type array of string.
         /// </summary>
-        [XmlElementAttribute( "EvtParam", Order = 1 )]
+        [XmlElementAttribute("EvtParam", Order=1)]
         public string[] EvtParam;
-
+        
         /// <summary>
         /// Element <b>EvtDesc</b> (Event Description) of type string.
         /// </summary>
-        [XmlElementAttribute( Order = 2 )]
+        [XmlElementAttribute(Order=2)]
         public string EvtDesc;
-
+        
         /// <summary>
         /// Element <b>EvtTm</b> (Event Time) of type System.DateTime.
         /// </summary>
-        [XmlElementAttribute( Order = 3 )]
+        [XmlElementAttribute(Order=3)]
         public System.DateTime EvtTm;
-
+        
         /// <summary>
         /// Element <b>EvtTmSpecified</b> (Event Time Specified) of type bool.
         /// </summary>
-        [XmlIgnoreAttribute()]
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
         public bool EvtTmSpecified;
     }
 }
