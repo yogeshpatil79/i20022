460c0b9cf3419e077350622ccafdf25479038aa5
cep.develop@gmail.com <cep.develop@gmail.com@1e891f1c-c86b-11dd-9042-6d746b85d38b>
2009-02-27 05:30:02 +0000

    v 1.0.0.2

--- src/ClassLibrary/TradeServicesManagement/tsmt_032_001_03.cs
+++ src/ClassLibrary/TradeServicesManagement/tsmt_032_001_03.cs
@@ -57,7 +57,7 @@ namespace i20022.tsmt03200103
     /// <summary>
     /// <b>Document</b> is the root class of the ISO 20022 message
     /// with id tsmt.032.001.03 name StatusExtensionNotificationV03 from Trade Services Management business area.
-    /// Repository:R 6.8.0.1 v492 - 4 July 2008
+    /// Repository:R 6.14.4.1 v524 - 8 December 2008
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
