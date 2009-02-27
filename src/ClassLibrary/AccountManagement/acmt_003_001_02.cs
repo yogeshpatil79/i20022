9c7580b6a608fbc67f4f14dfd9c4804c76929892
cep.develop <cep.develop@gmail.com>
2009-02-27 05:03:00 +0000

    v 1.0.0.2

--- src/ClassLibrary/AccountManagement/acmt_003_001_02.cs
+++ src/ClassLibrary/AccountManagement/acmt_003_001_02.cs
@@ -57,7 +57,7 @@ namespace i20022.acmt00300102
     /// <summary>
     /// <b>Document</b> is the root class of the ISO 20022 message
     /// with id acmt.003.001.02 name AccountModificationInstructionV02 from Account Management business area.
-    /// Repository:R 6.8.0.1 v492 - 4 July 2008
+    /// Repository:R 6.14.4.1 v524 - 8 December 2008
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
