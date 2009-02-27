edca4e28ce54ef784ac32444d082b1a5860c71c9
cep.develop@gmail.com <cep.develop@gmail.com@1e891f1c-c86b-11dd-9042-6d746b85d38b>
2009-02-27 05:07:30 +0000

    v 1.0.0.2

--- src/ClassLibrary/PaymentsClearingAndSettlement/pacs_008_001_01.cs
+++ src/ClassLibrary/PaymentsClearingAndSettlement/pacs_008_001_01.cs
@@ -57,7 +57,7 @@ namespace i20022.pacs00800101
     /// <summary>
     /// <b>Document</b> is the root class of the ISO 20022 message
     /// with id pacs.008.001.01 name FIToFICustomerCreditTransferV01 from Payments Clearing and Settlement business area.
-    /// Repository:R 6.8.0.1 v492 - 4 July 2008
+    /// Repository:R 6.14.4.1 v524 - 8 December 2008
     /// </summary>
     [SerializableAttribute()]
     [DebuggerStepThroughAttribute()]
